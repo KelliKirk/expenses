7b91695ee3a15718eebd6d54b67c5236196d8033
Kelli Kirk <kelli.kirk@voco.ee>
2024-11-22 11:43:20 +0200

    Komponendi jaotamine mitmeks - algus

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,7 +3,11 @@ import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
-    console.log(props);
+    console.log(props.date);
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'} )
+    const month = props.date.toLocaleString('en-US', {month: 'long'} )
+    const year = props.date.getFullYear()
+    
     return (
         <div className='expense-item'>
             <ExpenseDate date={props.date} />
