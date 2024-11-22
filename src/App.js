78911ca49a3b4f04dca25825ec3f76fb0afd2cd1
Kelli Kirk <kelli.kirk@voco.ee>
2024-11-22 11:36:34 +0200

    Komponendi andmete sisestamine

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import logo from './logo.svg';
+// App.js
 import './App.css';
 import ExpenseItem from './components/ExpenseItem';
-import ExpenseDate from './components/ExpenseDate';
 
 function App() { 
-  const data = {
-    date: new Date(2024, 10, 22),
-    title: 'New Book',
-    price: 39.99
-  } 
-  const expenses = {
-    date: new Date(2024, 10, 22),
-    title: 'New Book',
-    price: 39.99
-  } 
+  // Defineeri expenses kui massiiv
+  const expenses = [
+    {
+      date: new Date(2024, 10, 22),
+      title: 'New Book',
+      price: 39.99
+    },
+    {
+      date: new Date(2024, 10, 23),
+      title: 'Another Book',
+      price: 29.99
+    }
+  ]; 
   
   return (
     <div className="App">
-      <ExpenseItem  data ={expenses[0]}   />
-
-<ExpenseItem data = {expenses[1]} />
-    
+      <ExpenseItem 
+        date={expenses[0].date}
+        title={expenses[0].title}
+        price={expenses[0].price}
+      />
+      
+      <ExpenseItem 
+        date={expenses[1].date}
+        title={expenses[1].title}
+        price={expenses[1].price}
+      />
     </div>
   );
 }
 
 export default App;
+     
+      
+
+    
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,18 @@
-import './ExpenseItem.css'
+// ExpenseItem.js
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
-const ExpenseItem = (props) => {
-   console.log(props.data)
-return (<div className = 'expense-item'>
-    <div>{props.data.date} </div>
-    <div className ='expense-item__description'>
-<h2>{props.data.title}</h2>
-<div className = 'expense-item__price'>{props.data.price}</div>
-</div>
-</div>)
-} 
+function ExpenseItem(props) {
+    console.log(props);
+    return (
+        <div className='expense-item'>
+            <ExpenseDate date={props.date} />
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.price}</div>
+            </div>
+        </div>
+    );
+}
 
-export default ExpenseItem
+export default ExpenseItem;
