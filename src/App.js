a723fc8bcd1d76d1fa025fc49ceb4054af982d82
Kelli Kirk <kelli.kirk@voco.ee>
2024-11-22 10:35:23 +0200

    Ylesanne 1 - komponendi loomine ja kujundamine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import ExpenseItem from './components/ExpenseItem';
+import ExpenseDate from './components/ExpenseDate';
 
 function App() {
   return (
     <div className="App">
       <ExpenseItem />
+      <ExpenseDate />
+      <ExpenseItem />
+      <ExpenseDate />
     </div>
   );
 }
---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,11 @@
+import './ExpenseDate.css'
+
+const ExpenseDate = () => {
+    return (<div className = 'expense-date'>
+        <div className = 'expense-date__month'>Month</div>
+        <div className = 'expense-date__day'>Day</div>
+        <div className = 'expense-date__year'>Year</div>
+        </div>)
+    } 
+    
+    export default ExpenseDate
