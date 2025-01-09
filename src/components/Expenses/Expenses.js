223d10788bb80a2e8b5477b0e92c6f464af37c6f
Kelli Kirk <kelli.kirk@voco.ee>
2025-01-09 12:13:15 +0200

    Year data filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,25 @@
 import ExpenseItem from "./ExpenseItem"
 import Card from "../UI/Card"
 import './Expenses.css'
+import ExpensesFilter from './ExpensesFilter'
+import { useState } from 'react'
+
 
 const Expenses = (props) => {
+    // useState filtreerimisaasta jaoks
+    const [selectedYear, setSelectedYear] = useState('2024')
+
+    // filtri muutuse handler
+    const filterChangeHandler = (selectedYear) => {
+        console.log('Year data in Expenses.js:', selectedYear);
+        setSelectedYear(selectedYear);
+    }
+
     return (
-            <Card className="expenses">
+            <Card className="expenses"> 
+                <ExpensesFilter selected={selectedYear}
+                onChangeFilter={filterChangeHandler}
+                />  
                 <ExpenseItem data={props.expenses[0]} />
                 <ExpenseItem data={props.expenses[1]} />
             </Card>
