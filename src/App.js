9b41ce1638f3515ac9f2d28ff04846068f6c21d5
Kelli Kirk <kelli.kirk@voco.ee>
2025-01-09 13:41:13 +0200

    Form data use in lists

--- src/App.js
+++ src/App.js
@@ -1,33 +1,49 @@
 import './App.css';
 import Expenses from './components/Expenses/Expenses';  
 import NewExpense from './components/NewExpense/NewExpense'
+import { useState } from 'react'
 
 
 
 const App = () => {
+
   const DYMMY_EXPENSES = [
     {
       id: 'id1',
       date: new Date(2024, 10, 22),
       title: 'New Book',
-      amount: 19.20
+      amount: parseFloat(19.20) // parseFloat teeb stringist komakohaga arvu
     },
     {
       id: 'id2',
       date: new Date(2024, 10, 22),
       title: 'New Bag',
-      amount: 99.99
-    }
+      amount: parseFloat(99.99)
+    },
+    {
+      id: 'id3',
+      date: new Date(2024, 10, 22),
+      title: 'New Jeans',
+      amount: parseFloat(50.00)
+    },
   ];  
  
+  const [expenses, setExpenses] = useState(DYMMY_EXPENSES)
+
   const addExpenseHandler = (expense) => {
+    setExpenses((previousExpenses) => {
+      return [expense, ...previousExpenses] 
+    } )
+
     console.log('In App.js')
     console.log(expense)
+    setExpenses([expense, ...expenses])
   } 
+
   return (
     <div className="App">
       <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
-      <Expenses expenses={DYMMY_EXPENSES} /> 
+      <Expenses expenses={expenses} /> 
     </div>
   );
 }
