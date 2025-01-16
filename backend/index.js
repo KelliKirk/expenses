b7c3493d2f5f6e50b80a1980914eb466d5d20c78
Kelli Kirk <kelli.kirk@voco.ee>
2025-01-16 09:37:40 +0200

    HTTP requests

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,22 @@
 import express from 'express'
+import fs from 'node:fs/promises'
 
 const app = express()
+
+
+// Middleware (app.use(...)) seadistab päised CORS-iks (Cross-Origin Resource Sharing), et päringud oleks lubatud ka teistelt domeenidelt.
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+    next()
+} )
+
+app.get('/expenses', async(req, res) => {
+    const fileContent = await fs.readFile('./data/expenses.json')
+    const expensesData = JSON.parse(fileContent)
+    res.status(200).json({expenses: expensesData} ) // andmete saatmine kliendile
+} ) 
 
 app.listen(3005, () => {
     console.log('backend server connected')
